a37d422bed6dce6b0b77d4e62f5d7eb4d60b81ed
T3NED <software@t3ned.com>
2021-12-12 19:24:51 +0000

    feat: :sparkles: add database provider

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,5 +4,8 @@ export * from "./errors/MissingEnvVariableError";
 // Config
 export * as config from "../config";
 
+// Providers
+export * from "./providers/DatabaseProvider";
+
 // Utils
 export * from "./utils/loggers";
---
+++ src/lib/providers/DatabaseProvider.ts
@@ -0,0 +1,47 @@
+import { databaseLogger, config } from "#lib";
+import { PrismaClient } from "@prisma/client";
+
+export class DatabaseProvider {
+  /**
+   * The PrismaClient instance.
+   */
+  public client: PrismaClient;
+
+  public constructor() {
+    const client = new PrismaClient({
+      datasources: {
+        db: {
+          url: config.uri.postgresql
+        }
+      },
+      errorFormat: "pretty",
+      rejectOnNotFound: {
+        findFirst: false,
+        findUnique: false
+      },
+      log: [
+        { level: "info", emit: "event" },
+        { level: "warn", emit: "event" },
+        { level: "error", emit: "event" }
+      ]
+    });
+
+    client.$use(async (params, next) => {
+      const startTime = Date.now();
+      const result = await next(params);
+      const timeTook = Date.now() - startTime;
+
+      databaseLogger.info(`Query ${params.model}.${params.action} took ${timeTook}ms`);
+
+      return result;
+    });
+
+    client.$on("info", (data) => databaseLogger.info(data.message));
+    client.$on("warn", (data) => databaseLogger.warn(data.message));
+    client.$on("error", (data) => databaseLogger.error(data.message));
+
+    this.client = client;
+  }
+}
+
+export const databaseProvider = new DatabaseProvider();
