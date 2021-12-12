9d547ebaa30c8c5ed4906afb8821754a6e128182
T3NED <software@t3ned.com>
2021-12-12 23:18:48 +0000

    feat: :sparkles: add database helpers for all the tables

--- src/lib/providers/helpers/DatabaseHelpers.ts
+++ src/lib/providers/helpers/DatabaseHelpers.ts
@@ -1,3 +1,4 @@
+import type { Guild, ModerationTask, Modlog, Prisma } from "@prisma/client";
 import type { DatabaseProvider } from "#lib";
 
 export class DatabaseHelpers {
@@ -19,4 +20,220 @@ export class DatabaseHelpers {
   public get client() {
     return this._provider.client;
   }
+
+  /**
+   * Finds a guild by id
+   * @param guildId The id of the guild
+   * @param withModlogs Whether to include modlogs
+   */
+  public findGuild(guildId: string, withModlogs: true): Promise<GuildWithModlogs | null>;
+  /**
+   * Finds a guild by id
+   * @param guildId The id of the guild
+   * @param withModlogs Whether to include modlogs
+   */
+  public findGuild(guildId: string, withModlogs: false): Promise<Guild | null>;
+  /**
+   * Finds a guild by id
+   * @param guildId The id of the guild
+   * @param withModlogs Whether to include modlogs
+   */
+  public findGuild(
+    guildId: string,
+    withModlogs = false
+  ): Promise<Guild | GuildWithModlogs | null> {
+    return this.client.guild.findUnique({
+      include: {
+        modlogs: withModlogs
+      },
+      where: {
+        id: guildId
+      }
+    });
+  }
+
+  /**
+   * Ensures a guild exists
+   * @param guildId The id of the guild
+   */
+  public async ensureGuild(guildId: string): Promise<Guild> {
+    return this.client.guild.upsert({
+      where: {
+        id: guildId
+      },
+      create: {
+        id: guildId
+      },
+      update: {}
+    });
+  }
+
+  /**
+   * Updates a guild
+   * @param guildId The id of the guild
+   * @param data The data to update
+   */
+  public updateGuild(guildId: string, data: Prisma.GuildUpdateInput): Promise<Guild> {
+    return this.client.guild.update({
+      where: {
+        id: guildId
+      },
+      data
+    });
+  }
+
+  /**
+   * Deletes a guild
+   * @param guildId The id of the guild
+   */
+  public deleteGuild(guildId: string): Promise<Guild> {
+    return this.client.guild.delete({
+      where: {
+        id: guildId
+      }
+    });
+  }
+
+  /**
+   * Increments the guild modlog case id
+   * @param guildId The id of the guild
+   */
+  public async incrementGuildModlogCaseId(guildId: string): Promise<number> {
+    const guild = await this.ensureGuild(guildId);
+    const modlogCaseId = guild.modlogCaseId + 1;
+    await this.updateGuild(guildId, { modlogCaseId });
+    return modlogCaseId;
+  }
+
+  /**
+   * Finds a guild by id
+   * @param guildId The id of the guild
+   * @param caseId The id of the modlog case
+   */
+  public findModlog(guildId: string, caseId: number): Promise<ModlogWithTask | null> {
+    return this.client.modlog.findUnique({
+      include: {
+        task: true
+      },
+      where: {
+        modlogId: {
+          guildId,
+          caseId
+        }
+      }
+    });
+  }
+
+  /**
+   * Finds all the modlogs for an offender
+   * @param guildId The id of the guild
+   * @param offenderId The id of the offender
+   */
+  public findModlogsByOffender(
+    guildId: string,
+    offenderId: string
+  ): Promise<ModlogWithTask[]> {
+    return this.client.modlog.findMany({
+      include: {
+        task: true
+      },
+      where: {
+        guildId,
+        offenderId
+      }
+    });
+  }
+
+  /**
+   * Finds all the modlogs for a moderator
+   * @param guildId The id of the guild
+   * @param moderatorId The id of the moderator
+   */
+  public findModlogsByModerator(
+    guildId: string,
+    moderatorId: string
+  ): Promise<ModlogWithTask[]> {
+    return this.client.modlog.findMany({
+      include: {
+        task: true
+      },
+      where: {
+        guildId,
+        moderatorId
+      }
+    });
+  }
+
+  /**
+   * Creates a modlog
+   * @param guildId The id of the guild
+   * @param data The modlog create data
+   * @param duration The modlog create data
+   */
+  public async createModlog(
+    guildId: string,
+    data: Prisma.ModlogCreateInput,
+    duration?: number
+  ): Promise<ModlogWithTask> {
+    const modlogCaseId = await this.incrementGuildModlogCaseId(guildId);
+
+    const modlog = await this.client.modlog.create({
+      include: {
+        task: true
+      },
+      data: {
+        ...data,
+        caseId: modlogCaseId,
+        guild: {
+          connect: {
+            id: guildId
+          }
+        }
+      }
+    });
+
+    if (duration) {
+      const task = await this.createModerationTask(modlog.id, duration);
+      modlog.task = task;
+    }
+
+    return modlog;
+  }
+
+  /**
+   * Creates a moderation task
+   * @param modlogId The id of the modlog
+   * @param duration The duration of the task
+   */
+  public createModerationTask(
+    modlogId: string,
+    duration: number
+  ): Promise<ModerationTask> {
+    return this.client.moderationTask.create({
+      data: {
+        duration,
+        expiresAt: new Date(Date.now() + duration),
+        modlog: {
+          connect: {
+            id: modlogId
+          }
+        }
+      }
+    });
+  }
+
+  /**
+   * Deletes a moderation task
+   * @param modlogId The id of the modlog
+   */
+  public deleteModerationTask(modlogId: string): Promise<ModerationTask> {
+    return this.client.moderationTask.delete({
+      where: {
+        modlogId
+      }
+    });
+  }
 }
+
+export type GuildWithModlogs = Guild & { modlogs: Modlog[] };
+export type ModlogWithTask = Modlog & { task: ModerationTask | null };
