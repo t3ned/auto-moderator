c7a5ad006a2b34f5409317d51d0bead20efe31db
T3NED <software@t3ned.com>
2021-12-13 00:36:57 +0000

    feat: :sparkles: login to discord

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,22 @@
+import { ClientOptions, Intents, Options } from "discord.js";
+
 export enum Colors {
   Primary = 0x578af7,
   Success = 0x77dd77,
   Warning = 0xf5b426,
   Danger = 0xf45b69
 }
+
+export const clientOptions: ClientOptions = {
+  restTimeOffset: 0,
+  makeCache: Options.cacheWithLimits({
+    MessageManager: 100,
+    ThreadManager: 100,
+    UserManager: 10000
+  }),
+  intents: new Intents()
+    .add("GUILDS")
+    .add("GUILD_MEMBERS")
+    .add("GUILD_BANS")
+    .add("GUILD_MESSAGES")
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 import "dotenv/config";
 import "module-alias/register";
+
+import { Client, config, consts } from "#lib";
+
+const client = new Client(consts.clientOptions);
+client.login(config.uri.discord);
