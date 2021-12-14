9589021e3bbcfe2615de3c49c50efe8432e2d56d
T3NED <software@t3ned.com>
2021-12-14 17:08:46 +0000

    refactor: :fire: remove modlog reasons array

--- src/constants.ts
+++ src/constants.ts
@@ -7,8 +7,6 @@ export enum Colors {
   Danger = 0xf45b69
 }
 
-export const modlogReasons: string[] = [];
-
 export const clientOptions: ClientOptions = {
   restTimeOffset: 0,
   makeCache: Options.cacheWithLimits({
