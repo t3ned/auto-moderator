ef4e8262f76579ebe66ecac4ca1b091b3a3ddade
T3NED <software@t3ned.com>
2021-12-14 18:21:28 +0000

    feat: :sparkles: add map for reversing moderation actions

--- src/lib/struct/moderator/ModerationShared.ts
+++ src/lib/struct/moderator/ModerationShared.ts
@@ -1,4 +1,4 @@
-import type { Prisma, Guild, Modlog, ModerationTask } from "@prisma/client";
+import { Prisma, Guild, Modlog, ModlogCaseType, ModerationTask } from "@prisma/client";
 
 export interface ModlogUser {
   id: string;
@@ -12,6 +12,15 @@ export enum ModlogReason {
 export const ModlogReasons = new Map<ModlogReason, string>([
   [ModlogReason.SPAM, "Spam is not allowed anywhere in this server."]
 ]);
+
+export const reversedModlogActionMap: Record<ModlogCaseType, ModlogCaseType | null> = {
+  [ModlogCaseType.WARN]: null,
+  [ModlogCaseType.KICK]: null,
+  [ModlogCaseType.MUTE]: ModlogCaseType.UNMUTE,
+  [ModlogCaseType.UNMUTE]: null,
+  [ModlogCaseType.BAN]: ModlogCaseType.UNBAN,
+  [ModlogCaseType.UNBAN]: null
+};
 
 // eslint-disable-next-line prettier/prettier
 export type ModlogData = Omit<Prisma.ModlogCreateInput, "moderatorId" | "offenderId" | "caseId" | "guild"> 
