bc33f73153da5415580445287176645aac393875
T3NED <software@t3ned.com>
2021-12-14 16:55:00 +0000

    feat: :sparkles: add warn moderation method

--- src/lib/struct/moderator/ModerationActions.ts
+++ src/lib/struct/moderator/ModerationActions.ts
@@ -1,7 +1,27 @@
-import { ModerationBase } from "#lib";
+import { ModerationBase, ModerationLog, ModlogReason, dangerEmbed } from "#lib";
+import type { GuildMember, User } from "discord.js";
+import { ModlogCaseType } from "@prisma/client";
 
 export class ModerationActions extends ModerationBase {
-  // public warn() {}
+  /**
+   * Warns a member
+   * @param member The offending member
+   * @param moderator The responsible moderator
+   * @param reason The reason for this warning
+   */
+  public async warn(member: GuildMember, moderator: User, reason: ModlogReason) {
+    await this.manager.utils.tryDm(member.id, {
+      embeds: [
+        dangerEmbed(this.manager.utils.getReasonString(reason)).setAuthor("Warning")
+      ]
+    });
+
+    return new ModerationLog(member.guild, moderator, member.user, {
+      caseType: ModlogCaseType.WARN,
+      reason: reason
+    }).new();
+  }
+
   // public kick() {}
   // public mute() {}
   // public unmute() {}
