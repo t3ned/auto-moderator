e78e23d7984936b19471178e5c1e4dd754425a3a
T3NED <software@t3ned.com>
2021-12-15 19:41:58 +0000

    refactor: :truck: rename moderator dir to moderation

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -12,15 +12,15 @@ export * from "./providers/DatabaseProvider";
 export * from "./providers/RedisProvider";
 
 // Struct
-export * from "./struct/moderator/ModerationManager";
-export * from "./struct/moderator/ModerationBase";
-export * from "./struct/moderator/ModerationLog";
-export * from "./struct/moderator/ModerationActions";
-export * from "./struct/moderator/ModerationHistory";
-export * from "./struct/moderator/ModerationPendingAction";
-export * from "./struct/moderator/ModerationScheduler";
-export * from "./struct/moderator/ModerationUtils";
-export * from "./struct/moderator/ModerationShared";
+export * from "./struct/moderation/ModerationManager";
+export * from "./struct/moderation/ModerationBase";
+export * from "./struct/moderation/ModerationLog";
+export * from "./struct/moderation/ModerationActions";
+export * from "./struct/moderation/ModerationHistory";
+export * from "./struct/moderation/ModerationPendingAction";
+export * from "./struct/moderation/ModerationScheduler";
+export * from "./struct/moderation/ModerationUtils";
+export * from "./struct/moderation/ModerationShared";
 export * from "./struct/monitor/Monitor";
 export * from "./struct/monitor/MonitorHandler";
 export * from "./struct/SequentialQueue";
